bf4975b68ef01192c3d155591b0988ea2d85d101
ArcheGalla <artempushkar@gmail.com>
2016-04-20 08:55:48 +0300

    init commit

---
+++ src/app/index.vendor.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// node_modules
+import "angular";
+
+import "angular-ui-router";
+
+
+import 'jquery';
+
+
+
+import "jquery";
+
+
+
+// local scripts
+//import "../assets/js/...";
