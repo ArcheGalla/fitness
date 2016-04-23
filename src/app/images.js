3a934c821035ef261e74a6a1aebe7d0f43f5e87d
ArcheGalla <artempushkar@gmail.com>
2016-04-23 21:10:54 +0300

    organizers done

--- src/app/images.js
+++ src/app/images.js
@@ -1,16 +1,11 @@
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/hand-freeze.png';
 
 import '!!file-loader?name=[name].[ext]!../assets/images/logos/logo.png';
-
 import '!!file-loader?name=[name].[ext]!../assets/img/transparent.png';
-
-
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/screens/menu.jpg';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/screens/weather.jpg';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/screens/signup.jpg';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/iphone-freeze.png';
-
-
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/family-freeze.png';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/freeze-slide2.png';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/hand-freeze.png';
@@ -19,11 +14,14 @@ import '!!file-loader?name=[name].[ext]!../assets/img/freeze/screens/calendar.jp
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/screens/options.jpg';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/screens/sales.jpg';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/freeze-angled2.png';
-
 import '!!file-loader?name=[name].[ext]!../assets/js/rs-plugin/assets/large_left.png';
 import '!!file-loader?name=[name].[ext]!../assets/js/rs-plugin/assets/large_right.png';
-
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/hand-freeze.png';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/hand-freeze.png';
 import '!!file-loader?name=[name].[ext]!../assets/img/freeze/Slides/hand-freeze.png';
 
+
+// orgs
+import '!!file-loader?name=[name].[ext]!../assets/images/organizers/anna_lashchuk.png';
+import '!!file-loader?name=[name].[ext]!../assets/images/organizers/sergey_konyushok.png';
+import '!!file-loader?name=[name].[ext]!../assets/images/organizers/sergey_putsov.png';
