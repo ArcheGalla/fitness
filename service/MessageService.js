637ef24dacb60165dd100974ce93e348ab4d2c29
ArcheGalla <artempushkar@gmail.com>
2016-05-16 00:27:53 +0300

    basic impl of messages

--- service/MessageService.js
+++ service/MessageService.js
@@ -1,7 +1,30 @@
 const nodemailer = require('nodemailer');
+const emails = (require('./emails.json')).toString();
+
+var transporter = nodemailer.createTransport('SMTP', {
+  service: "Gmail",
+  auth: {
+    user: "retinalviv@gmail.com",
+    pass: "REtinaLViv02"
+  }
+});
 
 module.exports.sendMessage = function (data) {
+  // setup e-mail data with unicode symbols
+  let mailOptions = {
+    from: 'dev test',
+    to: emails,
+    subject: `from user`,
+    text: `email message`
+  };
+
   return new Promise(function (resolve, reject) {
-    resolve('ok');
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (err) {
+        return reject();
+      }
+
+      return resolve(info);
+    });
   });
 };
