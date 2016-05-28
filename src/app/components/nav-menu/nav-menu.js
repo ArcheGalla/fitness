be59cc1af99a7745df946aad5850827f4dba2ea0
ArcheGalla <artempushkar@gmail.com>
2016-05-28 23:14:26 +0300

    add phone style for main block

--- src/app/components/nav-menu/nav-menu.js
+++ src/app/components/nav-menu/nav-menu.js
@@ -6,8 +6,7 @@ export default function (module) {
         <div class="container">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
               <span class="fa fa-bars fa-lg"></span>
             </button>
             <a class="navbar-brand" href="#top">
