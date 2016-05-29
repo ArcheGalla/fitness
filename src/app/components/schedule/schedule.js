628a8c5be46f2ed4d9ef43e2310caf7294a7b0eb
ArcheGalla <artempushkar@gmail.com>
2016-05-29 20:36:07 +0300

    basic impl of schedule switcher

--- src/app/components/schedule/schedule.js
+++ src/app/components/schedule/schedule.js
@@ -1,61 +1,85 @@
+import './schedule.scss';
+
+const schedule_day_one = require('./day_one.json');
+const schedule_day_two = require('./day_two.json');
+
 export default function (module) {
   module.directive('fitnessSchedule', function () {
     return ({
       template: `
-       <section class="schedule" id="screens" ng-controller="scheduleCtrl as schedule">
-        <div class="container">
-          <div class="section-heading scrollpoint sp-effect3">
-            <h1>Розклад</h1>
-            <div class="divider"></div>
-            <p>Розклад уточнюється</p>
-          </div>
-
-          <div class="filter scrollpoint sp-effect3" ng-if="false">
-            <a href="javascript:void(0)" ng-click="schedule.setSchedule(schedule.SCHEDULES.ALL)" class="button"
-               ng-class="{ 'active' : schedule.SCHEDULES.ALL === schedule.currertSchedule }">Всі дні</a>
-            <a href="javascript:void(0)" ng-click="schedule.setSchedule(schedule.SCHEDULES.ONE)" class="button"
-               ng-class="{ 'active' : schedule.SCHEDULES.ONE === schedule.currertSchedule }">Перший день</a>
-            <a href="javascript:void(0)" ng-click="schedule.setSchedule(schedule.SCHEDULES.TWO)" class="button"
-               ng-class="{ 'active' : schedule.SCHEDULES.TWO === schedule.currertSchedule }">Другий день</a>
-          </div>
-
-        </div>
-        <div class="schedule-table" ng-if="false">
-          <div class="container-fluid">
-            <div class="row">
-              <!--<div class="col-lg-12">-->
-              <!--<div class="section-heading">-->
-              <!--<h3>Сцена 1</h3>-->
-              <!--<div class="divider"></div>-->
-              <!--&lt;!&ndash;<p>See what’s included in the App</p>&ndash;&gt;-->
-              <!--</div>-->
-              <!--</div>-->
-              <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 scene">
-                <div class="schedule-row" ng-if="schedule.SCHEDULES.ALL === schedule.currertSchedule">
-                  <span>08.00 - 09.00</span>
-                  <span>about schedule</span>
-                  <span>some info</span>
-                </div>
+           <section class="schedule" id="screens" ng-controller="scheduleCtrl as schedule">
+            <div class="container">
+              <div class="section-heading scrollpoint sp-effect3">
+                <h1>Розклад</h1>
+                <div class="divider"></div>
+                <p>Розклад уточнюється</p>
               </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 scene">
-                <div class="schedule-row" ng-if="schedule.SCHEDULES.ONE === schedule.currertSchedule">
-                  <span>08.00 - 09.00</span>
-                  <span>about schedule</span>
-                  <span>second</span>
-                </div>
+
+              <div class="filter scrollpoint sp-effect3">
+                <a href="javascript:void(0)" ng-click="schedule.setSchedule(schedule.SCHEDULES.ALL)" class="button"
+                   ng-class="{ 'active' : schedule.SCHEDULES.ALL === schedule.currertSchedule }">Всі дні</a>
+                <a href="javascript:void(0)" ng-click="schedule.setSchedule(schedule.SCHEDULES.ONE)" class="button"
+                   ng-class="{ 'active' : schedule.SCHEDULES.ONE === schedule.currertSchedule }">Перший день</a>
+                <a href="javascript:void(0)" ng-click="schedule.setSchedule(schedule.SCHEDULES.TWO)" class="button"
+                   ng-class="{ 'active' : schedule.SCHEDULES.TWO === schedule.currertSchedule }">Другий день</a>
               </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 scene">
-                <div class="schedule-row" ng-if="schedule.SCHEDULES.TWO === schedule.currertSchedule">
-                  <span>08.00 - 09.00</span>
-                  <span>about schedule</span>
-                  <span>third</span>
+
+            </div>
+
+            <div class="schedule-table" ng-if="schedule.SCHEDULES.ALL === schedule.currertSchedule">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 scene">
+                    <div class="schedule-row">
+                      <span>08.00 - 09.00</span>
+                      <span>about schedule</span>
+                      <span>some info</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-    </section>
-      `
+            <div class="schedule-table" ng-if="schedule.SCHEDULES.ONE === schedule.currertSchedule">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 scene" ng-repeat="scene in dayone">
+                    <div class="schedule-row" ng-repeat="item in scene.lectures">
+                      <span>{{ item.time }}</span>
+                      <span>{{ item.name }}</span>
+                      <span>{{ item.instructor }}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="schedule-table" ng-if="schedule.SCHEDULES.TWO === schedule.currertSchedule">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 scene" ng-repeat="scene in daytwo">
+                    <div class="schedule-row" ng-repeat="item in scene.lectures">
+                      <span>{{ item.time }}</span>
+                      <span>{{ item.name }}</span>
+                      <span>{{ item.instructor }}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </section>
+      `,
+      controller: function ($scope) {
+        $scope.dayone = schedule_day_one;
+        $scope.daytwo = schedule_day_two;
+      }
     });
   });
 }
+
+
+/*<!--<div class="col-lg-12">-->
+ <!--<div class="section-heading">-->
+ <!--<h3>Сцена 1</h3>-->
+ <!--<div class="divider"></div>-->
+ <!--&lt;!&ndash;<p>See what’s included in the App</p>&ndash;&gt;-->
+ <!--</div>-->
+ <!--</div>-->*/
