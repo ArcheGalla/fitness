bc18431a484f90abf6886ad5c2ce8fa56073867e
Arche Galla <artempushkar@gmail.com>
2016-06-03 09:51:09 +0300

    comment partners

--- src/app/components/partnership/partnership.js
+++ src/app/components/partnership/partnership.js
@@ -9,13 +9,7 @@ export default function (module) {
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
               <div class="partnership-align" direction="column">
                 <h2 class="text-left">Стати нашим партнером легко !</h2>
-                <p class="text-left ">
-                  sd ad asd asd ads  asdasdasd asdas das a dasdds
-                  sd ad asd asd ads  asdasdasd asdas das a dasdds
-                  sd ad asd asd ads  asdasdasd asdas das a dasdds
-                  sd ad asd asd ads  asdasdasd asdas das a dasdds
-                  sd ad asd asd ads  asdasdasd asdas das a dasdds
-                  sd ad asd asd ads  asdasdasd asdas das a dasdds
+                <p class="text-left">
                 </p>
               </div>
             </div>
