3d2c36c379e096357f3eafb62ce9795728aaa201
Arche Galla <artempushkar@gmail.com>
2016-06-03 21:00:16 +0300

    fix footer

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -1,27 +1,7 @@
 export default function (module) {
   module.directive('fitnessFooter', function () {
     return ({
-      template: `
-      <footer>
-        <div class="container">
-          <a href="#" class="scrollpoint sp-effect3">
-            <img src="logo.png" alt="" class="logo">
-          </a>
-          <div class="social">
-            <a href="https://www.youtube.com/channel/UCC_pPGQBC69uzt1tOrYhQkw" target="_blank"
-               class="scrollpoint sp-effect3"><i class="fa  fa-youtube-square fa-lg"></i></a>
-            <a href="https://vk.com/public99211814" target="_blank" class="scrollpoint sp-effect3"><i
-                class="fa fa-vk fa-lg"></i></a>
-            <a href="https://www.facebook.com/groups/776830325762913" target="_blank" class="scrollpoint sp-effect3"><i
-                class="fa fa-facebook fa-lg"></i></a>
-          </div>
-          <div class="rights">
-            <p>Copyright &copy; 2016</p>
-            <p>Lviv Fitness Weekend</p>
-          </div>
-        </div>
-      </footer>
-      `
+      templateUrl: require('./footer.html')
     });
   });
 }
