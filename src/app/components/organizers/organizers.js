f57a1ca90391eefb4ec5ddebd12cc8c63cc4f43f
Arche Galla <artempushkar@gmail.com>
2016-06-03 10:31:45 +0300

    switch position of organizers

--- src/app/components/organizers/organizers.js
+++ src/app/components/organizers/organizers.js
@@ -11,26 +11,24 @@ export default function (module) {
               <div class="divider"></div>
             </div>
             <div class="row">
-              <div class="col-md-4 col-sm-4 col-xs-12">
-                <div class="about-item scrollpoint sp-effect2">
-                  <img ng-src="{{ 'sergey_putsov.png' }}" alt="Сергій Пуцов">
-                  <h3>Сергій Пуцов</h3>
-                  <p>Головний тренер фітнес клубу «Skyfitness» (Київ).
-                    Кандидат наук фізичного виховання і спорту.
-                    Майстер спорту України з важкої атлетики.
-                    Співзасновник фітнес - проекту Oxygen Workout</p>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="about-item scrollpoint sp-effect1">
+                  <img ng-src="{{ 'sergey_konyushok.png' }}" alt="Сергій Конюшок">
+                  <h3>Сергій Конюшок</h3>
+                  <p>Керівник та співвласник мережі спортивних клубів «Олімп» (Львів).
+                    Майстер спорту України міжнародного класу.
+                    Чемпіон та рекордсмен світу зі стронгмену.
+                    Президент федерації стронгмену України</p>
                   <div class="social">
-                    <!--<a href="#" class="scrollpoint sp-effect3">-->
-                    <!--<i class="fa fa-twitter fa-lg"></i>-->
-                    <!--</a>-->
-                    <!--<a href="#" class="scrollpoint sp-effect3">-->
-                    <!--<i class="fa fa-google-plus fa-lg"></i>-->
-                    <!--</a>-->
-                    <a href="https://www.facebook.com/putsov.sergei" target="_blank" class="scrollpoint sp-effect3">
-                      <i class="fa fa-facebook fa-lg"></i></a>
+                    <!--<a href="#" class="scrollpoint sp-effect3"><i class="fa fa-twitter fa-lg"></i></a>-->
+                    <!--<a href="#" class="scrollpoint sp-effect3"><i class="fa fa-google-plus fa-lg"></i></a>-->
+                    <a href="https://www.facebook.com/sergiy.konyushok" target="_blank" class="scrollpoint sp-effect3">
+                      <i class="fa fa-facebook fa-lg"></i>
+                    </a>
                   </div>
                 </div>
               </div>
+              
               <div class="col-md-4 col-sm-4 col-xs-12">
                 <div class="about-item scrollpoint sp-effect5">
                   <img ng-src="{{ 'anna_lashchuk.png' }}" alt="Анна Лащук">
@@ -51,19 +49,22 @@ export default function (module) {
                 </div>
               </div>
               <div class="col-md-4 col-sm-4 col-xs-12">
-                <div class="about-item scrollpoint sp-effect1">
-                  <img ng-src="{{ 'sergey_konyushok.png' }}" alt="Сергій Конюшок">
-                  <h3>Сергій Конюшок</h3>
-                  <p>Керівник та співвласник мережі спортивних клубів «Олімп» (Львів).
-                    Майстер спорту України міжнародного класу.
-                    Чемпіон та рекордсмен світу зі стронгмену.
-                    Президент федерації стронгмену України</p>
+                <div class="about-item scrollpoint sp-effect2">
+                  <img ng-src="{{ 'sergey_putsov.png' }}" alt="Сергій Пуцов">
+                  <h3>Сергій Пуцов</h3>
+                  <p>Головний тренер фітнес клубу «Skyfitness» (Київ).
+                    Кандидат наук фізичного виховання і спорту.
+                    Майстер спорту України з важкої атлетики.
+                    Співзасновник фітнес - проекту Oxygen Workout</p>
                   <div class="social">
-                    <!--<a href="#" class="scrollpoint sp-effect3"><i class="fa fa-twitter fa-lg"></i></a>-->
-                    <!--<a href="#" class="scrollpoint sp-effect3"><i class="fa fa-google-plus fa-lg"></i></a>-->
-                    <a href="https://www.facebook.com/sergiy.konyushok" target="_blank" class="scrollpoint sp-effect3">
-                      <i class="fa fa-facebook fa-lg"></i>
-                    </a>
+                    <!--<a href="#" class="scrollpoint sp-effect3">-->
+                    <!--<i class="fa fa-twitter fa-lg"></i>-->
+                    <!--</a>-->
+                    <!--<a href="#" class="scrollpoint sp-effect3">-->
+                    <!--<i class="fa fa-google-plus fa-lg"></i>-->
+                    <!--</a>-->
+                    <a href="https://www.facebook.com/putsov.sergei" target="_blank" class="scrollpoint sp-effect3">
+                      <i class="fa fa-facebook fa-lg"></i></a>
                   </div>
                 </div>
               </div>
