e054f9f4f87c1a9b9eb9250053a94ef62dfaac1a
ArcheGalla <artempushkar@gmail.com>
2016-06-04 02:36:25 +0300

    fix widget move it to modal window

--- src/app/components/tickets/tickets.js
+++ src/app/components/tickets/tickets.js
@@ -1,17 +1,38 @@
-import "./tickets.scss";
+import './tickets.scss';
+import './widget.scss';
 export default function (module) {
   module.directive('fitnessTickets', function () {
     return ({
       templateUrl: require('./tickets.html'),
-      controller: function () {
+      controller: function ($modal) {
         var vm = this;
 
         vm.openWidget = function () {
-          console.log('work');
-          window.$('#2event_tickets_widget').css({
-            height: '400px',
-            width: '640px'
-          });
+          $modal
+            .open({
+              template: `
+              <div class="widget-modal">
+                <div id="2event_tickets_widget"></div>
+                <script type="text/javascript">
+                  var widgetOptions = {bg_color: 'fcfcfc', id: 669875, lang: 'uk',};
+                  (function () {
+                    var script = document.createElement('script');
+                    script.type = 'text/javascript';
+                    script.async = true;
+                    script.src = "https://2event.com/js/widget-tickets.js";
+                    document.getElementsByTagName('head')[0].appendChild(script);
+                  })();
+                </script>
+              </div>
+              `,
+              size: 'md',
+              windowClass: 'widget-modal',
+              animate: true,
+              controller: function () {
+                console.log('modal controller');
+              },
+              controllerAs: 'vm'
+            })
         }
       },
       controllerAs: 'vm'
