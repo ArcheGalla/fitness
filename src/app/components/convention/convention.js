562ba2c3bcc62336f7287b0bab635f063486a485
ArcheGalla <artempushkar@gmail.com>
2016-06-07 00:00:00 +0300

    add require presenters

--- src/app/components/convention/convention.js
+++ src/app/components/convention/convention.js
@@ -1,7 +1,11 @@
 export default function (module) {
   module.directive('fitnessConvention', function () {
     return ({
-      templateUrl: require('./convention.html')
+      templateUrl: require('./convention.html'),
+      controller: function () {
+        this.test = 'test';
+      },
+      controllerAs: 'vm'
     });
   });
 }
