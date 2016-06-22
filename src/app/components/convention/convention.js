f803b7b38a99646285a918830cdfd04e6ce71c38
ArcheGalla <artempushkar@gmail.com>
2016-06-22 11:13:26 +0300

    update count of presenters

--- src/app/components/convention/convention.js
+++ src/app/components/convention/convention.js
@@ -6,6 +6,7 @@ export default function (module) {
       templateUrl: require('./convention.html'),
       controller: function ($scope) {
         $scope.presenters  = require('../presenter/presenters.json');
+        console.log('$scope.presenters', $scope.presenters.length);
       }
     });
   });
