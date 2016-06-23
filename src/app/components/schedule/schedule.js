8dee6f6ddb8acbac89d1c8ea22a20b19c9ca6e25
ArcheGalla <artempushkar@gmail.com>
2016-06-24 01:26:58 +0300

    add schedule

--- src/app/components/schedule/schedule.js
+++ src/app/components/schedule/schedule.js
@@ -1,44 +1,41 @@
 import './schedule.scss';
 
-//const schedule_day_one = require('./day_one.json');
-//const schedule_day_two = require('./day_two.json');
-
-
 export default function (module) {
   module.directive('fitnessSchedule', function () {
     return ({
       templateUrl: require('./schedule.html'),
       controller: function ($scope) {
 
-        //$scope.dayone = schedule_day_one;
-        //$scope.daytwo = schedule_day_two;
+        $scope.dayone = require('./day_one.json');
+        $scope.daytwo = require('./day_two.json');
 
-        //const vm = this;
-        //
-        //vm.SCHEDULES = {
-        //  ONE: 'one',
-        //  TWO: 'two'
-        //};
-        //
-        //
-        //vm.currertSchedule = vm.SCHEDULES.ONE;
-        //
-        //vm.setSchedule = (day)=> {
-        //  switch (day) {
-        //    case vm.SCHEDULES.ONE:
-        //    {
-        //      vm.currertSchedule = vm.SCHEDULES.ONE;
-        //      break;
-        //    }
-        //    case vm.SCHEDULES.TWO:
-        //    {
-        //      vm.currertSchedule = vm.SCHEDULES.TWO;
-        //      break;
-        //    }
-        //  }
-        //};
+        const vm = this;
 
-      }
+        vm.SCHEDULES = {
+          ONE: 'one',
+          TWO: 'two'
+        };
+
+
+        vm.currertSchedule = vm.SCHEDULES.ONE;
+
+        vm.setSchedule = (day)=> {
+          switch (day) {
+            case vm.SCHEDULES.ONE:
+            {
+              vm.currertSchedule = vm.SCHEDULES.ONE;
+              break;
+            }
+            case vm.SCHEDULES.TWO:
+            {
+              vm.currertSchedule = vm.SCHEDULES.TWO;
+              break;
+            }
+          }
+        };
+
+      },
+      controllerAs: 'schedule'
     });
   });
 }
