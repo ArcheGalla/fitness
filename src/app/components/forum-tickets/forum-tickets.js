37fc1c548f5336be3d1b9ebdd8533b158246faa2
ArcheGalla <artempushkar@gmail.com>
2016-06-24 02:39:02 +0300

    add new sections

--- src/app/components/forum-tickets/forum-tickets.js
+++ src/app/components/forum-tickets/forum-tickets.js
@@ -4,9 +4,12 @@ export default function (module) {
   module.directive('forumTickets', function () {
     return ({
       templateUrl: require('./forum-tickets.html'),
+      scope: true,
       controller: function ($scope) {
         //$scope.TICKETS_ID = TICKETS_ID;
-        //$scope.openWidget = FitnessTicket.open;
+        $scope.openWidget = function () {
+
+        };
       }
     });
   });
