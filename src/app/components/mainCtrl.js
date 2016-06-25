b9f1c27e7120cf859e18776f736908928df91ae5
Arche Galla <artempushkar@gmail.com>
2016-06-25 13:53:18 +0300

    fix class to be in to lines

--- src/app/components/mainCtrl.js
+++ src/app/components/mainCtrl.js
@@ -2,5 +2,8 @@ export default function (module) {
   module
     .controller('MainCtrl', function () {
 
-    });
+    })
+    .config(['$sceProvider', function ($sceProvider) {
+      $sceProvider.enabled(false);
+    }]);
 }
