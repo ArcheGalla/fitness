f797c3da76ed1ca8107f1cac73fd870892f9dd4c
ArcheGalla <artempushkar@gmail.com>
2016-07-17 11:56:53 +0300

    add title for forum schedule table

--- src/app/components/forum-schedule/forum-schedule.js
+++ src/app/components/forum-schedule/forum-schedule.js
@@ -6,38 +6,8 @@ export default function (module) {
       templateUrl: require('./forum-schedule.html'),
       scope: true,
       controller: function ($scope) {
-
-        //$scope.dayone = require('./forum-schedule-day-one.json');
-        $scope.daytwo = require('./forum-schedule-day-two.json');
-
-        //
-        //const vm = this;
-        //
-        //vm.SCHEDULES = {
-        //  ONE: 'one',
-        //  TWO: 'two'
-        //};
-        //
-        //
-        //vm.currertSchedule = vm.SCHEDULES.ONE;
-        //
-        //vm.setSchedule = (day)=> {
-        //  switch (day) {
-        //    case vm.SCHEDULES.ONE:
-        //    {
-        //      vm.currertSchedule = vm.SCHEDULES.ONE;
-        //      break;
-        //    }
-        //    case vm.SCHEDULES.TWO:
-        //    {
-        //      vm.currertSchedule = vm.SCHEDULES.TWO;
-        //      break;
-        //    }
-        //  }
-        //};
-
+        $scope.dayone = require('./forum-schedule-day-one.json');
       }
-      //controllerAs: 'forumSchedule'
     });
   });
 }
