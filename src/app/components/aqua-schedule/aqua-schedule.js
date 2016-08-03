47f350ab143564bc1efc62a5dfe28b618e48a756
ArcheGalla <artempushkar@gmail.com>
2016-08-03 19:00:16 +0300

    add aqua presenters to main page

--- src/app/components/aqua-schedule/aqua-schedule.js
+++ src/app/components/aqua-schedule/aqua-schedule.js
@@ -1,3 +1,13 @@
-export default function (module) {
+import './aqua-schedule.scss';
 
+export default function (module) {
+  module.directive('aquaSchedule', function () {
+    return ({
+      restrict: 'E',
+      templateUrl: require('./aqua-schedule.html'),
+      controller: function ($scope) {
+        $scope.test = 'test';
+      }
+    });
+  });
 }
